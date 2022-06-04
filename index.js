19b7fbe9aca33cd5e1fa3cc2c0e17ddffc4bd96d
laugsg <lautaro.grucci@joingsg.com>
2022-06-04 20:15:00 +0200

    update > many reducer

--- index.js
+++ index.js
@@ -22,12 +22,17 @@ function createStore(reducer){
     }
 
     // 4. Update the state
-    /** dispatch
-     * Entrypoint of the state,
-     * implements a reducer for predictability.
-     * Then fires all subscriptions to notify updates.
+    /** Data _entrypoint_ to State
+     * implements a reducer to drive changes to state 
+     * based in action events. 
+     * fires subscriptions as notifying updates.
      */
     const dispatch = (action) => {
+        /** Reducer
+         * external exposure of state
+         * to let users _handle_ which data
+         * will be stored.
+         */
         state = reducer(state, action)
         listeners.forEach( listener => listener())
     }
@@ -46,15 +51,58 @@ function createStore(reducer){
 
 
 // App Code
-// reducers : updates handler based in action events (user interface)
+
+const ADD_TODO = 'ADD_TODO'
+const REMOVE_TODO = 'REMOVE_TODO'
+const TOGGLE_TODO = 'TOGGLE_TODO'
+const ADD_GOAL = 'ADD_GOAL'
+const REMOVE_GOAL = 'REMOVE_GOAL'
+
+// Action Creators
+function addTodoAction(todo){
+    return {
+        type: ADD_TODO,
+        todo
+    }
+}
+
+function removeTodoAction(id){
+    return {
+        type: REMOVE_TODO,
+        id
+    }
+}
+
+function toggleTodoAction(id){
+    return {
+        type: TOGGLE_TODO,
+        id
+    }
+}
+
+function addGoalAction(goal){
+    return {
+        type: ADD_GOAL,
+        goal
+    }
+}
+
+function removeGoalAction(id){
+    return {
+        type: REMOVE_GOAL,
+        id
+    }
+}
+
+// reducers _handler_ : drive changes to state based in actions
 function todos (state = [], action){
-    // actions event data driven
+    // actions : data event
     switch( action.type ){
-        case 'ADD_TODO':
+        case ADD_TODO:
             return state.concat([action.todo])
-        case 'REMOVE_TODO':
+        case REMOVE_TODO:
             return state.filter( todo => todo.id !== action.id )
-        case 'TOGGLE_TODO':
+        case TOGGLE_TODO:
             return state.map( todo => todo.id !== action.id ? todo : Object.assign({}, todo, {complete: !todo.complete}))
         default:
             return state
@@ -63,31 +111,58 @@ function todos (state = [], action){
 
 function goals (state = [], action){
     switch(action.type){
-        case 'ADD_GOAL':
+        case ADD_GOAL:
             return state.concat([action.goal])
-        case 'REMOVE_GOAL':
+        case REMOVE_GOAL:
             return state.filter( goal => goal.id !== action.id)
         default:
             return state
     }
 }
 
+function App (state = {}, action){
+    return {
+        todos: todos(state.todos, action),
+        goals: goals(state.goals, action)
+    }
+}
 
-const store = createStore(todos)
+
+const store = createStore(App)
 store.subscribe(() => {
     console.log(`The new state is: `, store.getState())
 })
 
-const unsubscribe = store.subscribe(() => {
-    console.log(`The store changed.`)
-})
-unsubscribe()
+// const unsubscribe = store.subscribe(() => {
+//     console.log(`The store changed.`)
+// })
+// unsubscribe()
 
-store.dispatch({
-    type: "ADD_TODO",
-    todo: {
-        id: 0,
-        name: "Learn React"
-    },
+// dispatch _entrypoint_
+store.dispatch(addTodoAction({
+    id: 0,
+    name: "Learn React",
     complete: false
-})
+}))
+
+store.dispatch(addTodoAction({
+    id: 1,
+    name: "Learn Redux",
+    complete: false
+}))
+
+store.dispatch(removeTodoAction(0))
+
+store.dispatch(toggleTodoAction(1))
+
+store.dispatch(addGoalAction({
+    id: 0,
+    name: "React Dev"
+}))
+
+store.dispatch(addGoalAction({
+    id: 1,
+    name: "Redux Dev"
+}))
+
+store.dispatch(removeGoalAction(0))
